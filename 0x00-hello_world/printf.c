8baea5a026d442075e2916adfb601f931b24a7fb
Stevenson001 <stevolala4all@gmail.com>
2022-09-08 17:26:31 -0700

    printf.c

---
+++ 0x00-hello_world/printf.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * takes no argument
+ *
+ * Description: print a string to standard output printf
+ * Return: return 0 for success
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcomes is a piece of art,\n");
+	return (0);
+}
+
