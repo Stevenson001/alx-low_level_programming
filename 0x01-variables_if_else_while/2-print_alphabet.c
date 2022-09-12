b59052a8da343b6a8351bfdea46a84393571ec0a
Stevenson001 <stevolala4all@gmail.com>
2022-09-12 07:58:02 -0700

    print alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+/**
+ *main - entry and exit point of program
+ *takes no arguments
+ *
+ *Description: print all letters of alphabet
+ *Return: 0 on success
+ */
+int main(void)
+
+{
+	int i;
+
+	for (i = 97; 1 < 123; i++)/*lowercase alpha in ascii*/
+		putchar(i);
+	putchar(10); /*newline ascii*/
+	return (0);
+}
