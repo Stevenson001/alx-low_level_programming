42a3105897032e04588234c494cf60175db0b862
Stevenson001 <stevolala4all@gmail.com>
2022-09-14 18:50:35 -0700

    2-alphabet_x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabet 10 times on newline.
+ * takes no arguments.
+ *
+ * Return: No specified return value.
+ */
+void print_alphabet_x10(void)
+{
+	int x, ch;
+
+	x = 1;
+	while (x <= 10)
+	{
+		ch = 97;
+		while (ch <= 122)
+		{
+			_putchar(ch);
+			ch++;
+		}
+		x++;
+		_putchar(10);
+	}
+}
