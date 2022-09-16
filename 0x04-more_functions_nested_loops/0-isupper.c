c705184868250ca31579453c3d6aa811c3cae9e4
Stevenson001 <stevolala4all@gmail.com>
2022-09-16 13:56:51 -0700

    0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _isupper - checks for uppercase characters.
+ * @c: Takes c as character arguement.
+ *
+ * Return: 1 is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+	if ((char)c >= 65 && (char)c <= 90)
+		return (1);
+	else
+		return (0);
+}
